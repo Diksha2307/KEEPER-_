a715295110223eea7bb41362373b40bf8647f9e8
Diksha 2307 <dikshasinghal239@gmail.com>
2021-05-23 17:05:44 +0530

    initial commits

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
+ReactDOM.render(
+
+    <App />,document.getElementById("root")
+);
+
+
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
